63e5f212acfb40f9a669136d5c5c98b1c6c19ca0
DreamLi <18829346477@163.com>
2020-03-21 15:20:13 +0800

    init project

---
+++ src/main/java/com/pssdev/pss/PssApplication.java
@@ -0,0 +1,13 @@
+package com.pssdev.pss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PssApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(PssApplication.class, args);
+   }
+
+}
