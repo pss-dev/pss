10d95f9b30c1d7183ff8d7c024a1f2f45df9b426
su1995 <suyang2678@qq.com>
2020-04-04 23:44:02 +0800

    添加eslint配置 添加首页组件

---
+++ web/src/views/inventory-index/components/index-header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="index-header">
+    <div class="header-left">进销存logo</div>
+    <div class="header-right">登出</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'IndexHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+  .index-header {
+    display: flex;
+    align-items: center;
+    padding-right: 20px;
+    height: 60px;
+    border-bottom: 1px solid #999;
+    .header-left {
+      width: 80%;
+      text-align: center;
+    }
+    .header-right {
+      width: 20%;
+    }
+  }  
+</style>
--- web/src/views/inventory-index/inventory-index.vue
+++ web/src/views/inventory-index/inventory-index.vue
@@ -1,15 +1,69 @@
 <template>
-  <div class="index">
-    
+  <div class="inventory-index">
+    <index-header></index-header>
+    <el-table
+      :data="tableData"
+      style="width: 100%;"
+    >
+      <el-table-column
+        v-for="item in columns"
+        v-bind="item"
+        :key="item.propVal"
+        width="180"
+      >
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
+import indexHeader from "./components/index-header";
 export default {
-
-}
+  components: {
+    indexHeader
+  },
+  data() {
+    return {
+      columns: [
+        {
+          label: "日期",
+          prop: "date"
+        },
+        {
+          label: "姓名",
+          prop: "name"
+        },
+        {
+          label: "地址",
+          prop: "address"
+        }
+      ],
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ]
+    };
+  },
+  methods() {}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
