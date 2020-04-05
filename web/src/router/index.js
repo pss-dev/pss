8db551937ea4c2c994ad2e3e2015ad0bfc6ba3ad
DreamLi <18829346477@163.com>
2020-04-05 11:50:25 +0800

    fix build failed.  clean node_modules  after build success.

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '@/views/inventory-index/inventory-index'
+import Department from '@/views/base-info/department-info/department-info'
 // import Login from '@/views/login'
 
 Vue.use(VueRouter)
@@ -13,6 +14,13 @@ export default new VueRouter({
         title: '首页'
       },
       component: Index
+    },
+    {
+      path: '/dept',
+      meata: {
+        title: 'Department'
+      },
+      component: Department
     }
   ]
 })
