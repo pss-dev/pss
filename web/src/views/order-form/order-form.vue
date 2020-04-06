2c2b6c0bbe84be10d04210088e3b09b7a0886798
llh-exercise <1421793864@qq.com>
2020-04-06 14:55:36 +0800

    添加 单据的folder 以及相关search dialog

---
+++ web/src/views/order-form/order-form.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <el-button @click="showDepartmentDialog">部门</el-button>
+
+    <department-search-dialog
+      v-if="departmentDialog">
+    </department-search-dialog>
+  </div>
+</template>
+
+<script>
+  import departmentSearchDialog from "../components/department-search-dialog.vue"
+
+  export default {
+    name: "orderForm",
+
+    components: {
+        "department-search-dialog": departmentSearchDialog
+    },
+
+    data() {
+      return {
+        departmentDialog: false,
+      }
+    },
+    
+    methods: {
+      showDepartmentDialog() {
+        this.departmentDialog = true;
+      }
+    },
+  }
+</script>
+
+<style>
+</style>
